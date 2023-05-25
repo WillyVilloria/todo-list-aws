73cfa3060e2e63b0f7d8e3c7a6feccba76d477b0
WillyVilloria <miguelcarter@gmail.com>
2023-05-25 20:40:07 +0000

    cambio unit_test

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -8,5 +8,5 @@ export DYNAMODB_TABLE=todoUnitTestsTable
 python test/unit/TestToDo.py
 pip show coverage
 coverage run --include=src/todoList.py test/unit/TestToDo.py -m
-coverage report
+coverage report --show-missing
 coverage xml
